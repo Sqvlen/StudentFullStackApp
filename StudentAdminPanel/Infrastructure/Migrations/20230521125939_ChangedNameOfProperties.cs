88c750eea38726c9613ddb5cbbb83434e644abc3
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-05-21 17:01:25 +0300

    ADD: Migrations
    ADD: Migrations

---
+++ StudentAdminPanel/Infrastructure/Migrations/20230521125939_ChangedNameOfProperties.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedNameOfProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "mobile",
+                table: "Students",
+                newName: "Mobile");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfileImageUrl",
+                table: "Students",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalAddress",
+                table: "Addresses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhysicalAddress",
+                table: "Addresses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Mobile",
+                table: "Students",
+                newName: "mobile");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfileImageUrl",
+                table: "Students",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalAddress",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhysicalAddress",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
