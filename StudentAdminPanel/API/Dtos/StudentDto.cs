088b680fce3306807114146ce0d1c357410f2a4b
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-05-23 02:09:01 +0300

    ADD: Dtos (address, base, gender, student)
    ADD: Dtos (address, base, gender, student)

---
+++ StudentAdminPanel/API/Dtos/StudentDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+
+namespace API.Dtos;
+
+public class StudentDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Email { get; set; }
+    public long Mobile { get; set; }
+    public string? ProfileImageUrl { get; set; }
+    
+    public Gender Gender { get; set; }
+    public Address Address { get; set; }
+}
