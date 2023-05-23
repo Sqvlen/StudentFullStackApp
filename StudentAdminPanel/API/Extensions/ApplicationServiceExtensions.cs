6b22a6f7480de53fd4689d773d25fd924a8eaab2
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-05-23 23:40:11 +0300

    ADD: ApplicationServiceExtensions
    ADD: ApplicationServiceExtensions

---
+++ StudentAdminPanel/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Core.Interfaces;
+using Infrastructure;
+using Infrastructure.Data;
+using Infrastructure.Repostories;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
+        });
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
+        services.AddDbContext<StudentPanelContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("StudentAdminPortalDb"));
+        });
+
+        return services;
+    }
+}
