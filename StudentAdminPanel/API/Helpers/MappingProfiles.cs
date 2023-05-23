7717a878026c6a1d82b9f0a0f7c44eadf62bb3b2
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-05-23 23:39:08 +0300

    UPD: Added mapping profiles for IMapper
    UPD: Added mapping profiles for IMapper

---
+++ StudentAdminPanel/API/Helpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Student, StudentDto>();
+        CreateMap<Gender, GenderDto>();
+        CreateMap<Address, AdressDto>();
+    }
+}
