b730fdca2816450cf8e61f418a687f267c4328da
Sqvlen <65315887+Sqvlen@users.noreply.github.com>
2023-05-23 23:40:29 +0300

    Update
    Update

--- StudentAdminPanel/API/Program.cs
+++ StudentAdminPanel/API/Program.cs
@@ -1,25 +1,24 @@
+using API.Extensions;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
+app.UseCors("CorsPolicy");
+app.UseSwaggerDocumention();
 app.MapControllers();
 
 app.Run();
